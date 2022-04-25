cb681dbac6c52122ee27351de26d886e6e20e3dc
Matchobas <maalexandria@hotmail.com>
2022-04-24 23:42:41 -0300

    All test pass

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -18,18 +18,19 @@ export class GamesRepository implements IGamesRepository {
       .select('games.title', 'title')
       .where(`games.title ILIKE :title`, { title: `%${param}%` })
       .getRawMany()
-      // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query('SELECT count(id) FROM games'); // Complete usando raw query
+    return this.repository.query('SELECT count(id) FROM games');
   }
 
+  // SELECT * FROM users_games_games AS ugg INNER JOIN user ON user.id = ugg.usersId WHERE ugg.gamesId = id  
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
       .createQueryBuilder('games')
       .where('games.id = :id', { id })
-      .getRawMany()
-      // Complete usando query builder
+      .relation(Game, 'users')
+      .of(id)
+      .loadMany()
   }
 }
